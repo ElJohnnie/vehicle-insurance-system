038b99efe15fab27a698373d0bfa4051bc7ca7fb
Jonatha Follmer <jonatha.follmer@compasso.com.br>
2023-09-08 20:07:17 -0300

    initial commit

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# Todo list
+
+## back-end
+
+- [] Consertar build docker
+
+## nginx
+
+- [] Consertar transpilação dos arquivos
---
+++ front-end/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: process.env.VUE_APP_DEV_ENVIRONMENT === false ? "/" : "/",
+  devServer: {
+    port: process.env.VUE_APP_PORT || 80,
+    host: process.env.VUE_APP_HOST,
+  },
+};
